d65e13111f830861654bd7c4ca138d224cbb3d28
C5hives <e1122945@u.nus.edu>
2025-07-09 16:22:22 +0800

    Add solution for problem 1394

---
+++ 1394_find-lucky-integer-in-an-array.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/find-lucky-integer-in-an-array/
+
+class Solution(object):
+    def findLucky(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        frequencies = dict()
+        for number in arr:
+            if number not in frequencies:
+                frequencies[number] = 0
+            frequencies[number] += 1
+        
+        largest_lucky_number = -1
+        for number, frequency in frequencies.items():
+            if number != frequency:
+                continue
+            
+            if number < largest_lucky_number:
+                continue
+            largest_lucky_number = number
+        
+        return largest_lucky_number
+        
